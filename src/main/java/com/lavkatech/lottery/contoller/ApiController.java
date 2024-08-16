395b7f7f062e4fe9485e301281c0a63d685cd0ae
MShinskiy <88272807+MShinskiy@users.noreply.github.com>
2024-08-16 16:56:32 +0200

    update

--- src/main/java/com/lavkatech/lottery/contoller/ApiController.java
+++ src/main/java/com/lavkatech/lottery/contoller/ApiController.java
@@ -3,14 +3,23 @@ package com.lavkatech.lottery.contoller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api")
 public class ApiController {
 
     @GetMapping("/lotteries")
-    public ResponseEntity<Object> getLotteryResult() {
+    public ResponseEntity<Object> getLotteryResult(@RequestParam String token) {
+        // TODO
+        return null;
+    }
 
+    @GetMapping("/users/{dtprf}")
+    public ResponseEntity<Object> getUserInfo(@RequestParam String token, @PathVariable String dtprf) {
+        // TODO
+        return null;
     }
 }
